06eca5607abe03cee554d679da932165377209de
Terence <terencegriff92@gmail.com>
2020-05-03 02:16:07 -0400

    Burger builder without backend

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import Aux from "../../../hoc/Auxiliary/Auxiliary";
+import Button from "../../UI/Button/Button";
+
+class OrderSummary extends Component {
+  // This could be a functional component
+  componentDidUpdate() {
+    console.log('[OrderSummary] Will Update')
+  }
+
+  render () {
+    const ingredientSummary = Object.keys(this.props.ingredients).map((ingKey) => {
+      return (
+        <li key={ingKey}>
+          <span style={{ textTransform: "capitalize" }}>{ingKey}</span> :{" "}
+          {this.props.ingredients[ingKey]}
+        </li>
+      );
+    });
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </Aux>
+    );
+  } 
+};
+
+export default OrderSummary;
